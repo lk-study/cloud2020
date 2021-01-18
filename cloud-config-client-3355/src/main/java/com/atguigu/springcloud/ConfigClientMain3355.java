db8ac12c627fdd427778ff57d5e4e84dd3ebab11
lk-study <likun@bjhzwq.com>
2021-01-18 10:07:00 +0800

    3344 3355 9527

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
